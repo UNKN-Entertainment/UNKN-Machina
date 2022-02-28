23e11858928e0c441d07771a5d3f506ed4942adc
renross <briross@udel.edu>
2022-02-28 13:37:20 -0500

    linter style changes

---
+++ config.example.ts
@@ -0,0 +1,35 @@
+export const BOT = {
+	TOKEN: '',					// Bot token 
+	CLIENT_ID: '',				// Application ID 
+	NAME: 'Machina'				// Bot Name
+};
+
+export const GUILD = '';	// Guild ID
+
+
+export const ROLES = {};		// Role IDS for each role
+
+export const CHANNELS = { // Channel IDs
+	ANNOUNCEMENTS: '',
+	ROLE_SELECT: '', 
+	MACHINA: '',
+	FEEDBACK: '',
+	ERROR_LOG: '',
+	SERVER_LOG: '',
+	MEMBER_LOG: '',
+	MOD_LOG: '',
+};
+
+export const ROLE_DROPDOWNS = {
+	AGE_ROLES: '',
+	PRONOUN_ROLES: '', 
+	STREAMER_ROLES: '',
+	PLATFORM_ROLES: '',
+	GAME_ROLES: '',
+	EVENT_ROLES: '',
+};
+
+export const GITHUB_TOKEN = '';
+export const GITHUB_PROJECT = '';
+export const PREFIX = 'm!';
+export const BLACKLIST = [];
