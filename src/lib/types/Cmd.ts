a16466237397fe20815023259b520a3488844cb3
renross <briross@udel.edu>
2022-03-01 20:31:16 -0500

    cmd and role types

---
+++ src/lib/types/Cmd.ts
@@ -0,0 +1,27 @@
+import { ROLES } from '@root/config';
+import { ApplicationCommandOptionData, ApplicationCommandPermissionData, CommandInteraction } from 'discord.js';
+
+export abstract class Command {
+
+	// members
+	name: string;
+	category: string;
+	enabled: boolean;
+	aliases?: Array<string>;
+	description: string;
+	usage?: string;
+	extendedHelp?: string;
+	runInDM?: boolean = true;
+	runInGuild?: boolean = true;
+	options?: ApplicationCommandOptionData[];
+
+	// functions
+	abstract run(interaction: CommandInteraction): Promise<unknown>;
+
+}
+
+export interface CompCommand {
+	name: string,
+	description: string,
+	options: ApplicationCommandOptionData[]
+}
