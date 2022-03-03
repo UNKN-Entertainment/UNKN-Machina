e80099c3b94f84f816b7a7586348a740ddaae15f
renross <briross@udel.edu>
2022-03-03 18:22:16 -0500

    updated example config

--- config.example.ts
+++ config.example.ts
@@ -1,18 +1,19 @@
 export const BOT = {
-	TOKEN: '',					// Bot token 
-	CLIENT_ID: '',				// Application ID 
-	NAME: 'Machina'				// Bot Name
+	TOKEN: '',		// Bot token
+	CLIENT_ID: '',	// Application ID 
+	NAME: ''		// Bot Name
 };
 
 export const GUILD = '';	// Guild ID
 
-export const ROLES = {	// Role IDS for each role
-	BOT_MASTER: '',
-	OPS: '', 
-	STAFF: '',
-};		
+export const ROLES = {
+	BOT_MASTER: '',	// User ID
+	OPPS: '',		// Role ID
+	STAFF: '',		// Role ID
+	MEMBER: ''		// Role ID
+};
 
-export const CHANNELS = { // Channel IDs
+export const CHANNELS = {	// Channel IDs
 	ANNOUNCEMENTS: '',
 	ROLE_SELECT: '', 
 	MACHINA: '',
@@ -23,7 +24,7 @@ export const CHANNELS = { // Channel IDs
 	MOD_LOG: '',
 };
 
-export const ROLE_DROPDOWNS = {
+export const ROLE_DROPDOWNS = {	// Message IDs
 	AGE_ROLES: '',
 	PRONOUN_ROLES: '', 
 	STREAMER_ROLES: '',
@@ -32,7 +33,6 @@ export const ROLE_DROPDOWNS = {
 	EVENT_ROLES: '',
 };
 
-export const GITHUB_TOKEN = '';
-export const GITHUB_PROJECT = '';
-export const PREFIX = 'm!';
+export const PREFIX = '';
 export const BLACKLIST = [];
+
